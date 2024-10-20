bb92f7b4d37f4f3c59f1317bb86ad9881b6c925e
Thang100705 <Thang10072005@gmail.com>
2024-10-20 12:31:14 +0700

    Add files via upload

---
+++ thiep-mung/config.js
@@ -0,0 +1,8 @@
+const nameGirl = '.';
+const giftUrl = 'https://www.facebook.com/arrogant2k5/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người được nhận';
+const contentCard = 'Chúc người nhận được nếu là nữ thì luôn vui vẻ tự tin lời chúc nhân ngày 20-10';
+const giftImage = 'https://png.pngtree.com/png-vector/20240322/ourlarge/pngtree-cute-cartoon-cat-png-image_12203528.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
